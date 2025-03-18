c02723b43d83df65b86c79fd2b43da45659e720f
lathifah199 <lathifahkhalifatunnisa.com>
2025-03-18 17:10:42 +0700

    Menambahkan List Item Controller dan View

---
+++ rentalmobil/resources/views/items.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar Item</title>
+</head>
+<body>
+    <h2>Ini adalah halaman list item mobil</h2>
+    <h3>1. BMW</h3>
+    <h3>2. Lamborgini</h3>
+    <h3>3. Toyota</h3>
+</body>
+</html>
--- rentalmobil/routes/web.php
+++ rentalmobil/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ItemController;
+
+// Route untuk ItemsController
+Route::get('/items', [ItemController::class, 'index']);
 
 // Route untuk DashboardController
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
