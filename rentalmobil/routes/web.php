4f396dc56d161c400014e38a85dee89330177301
kaxrinn <kaxrinnberry@gmail.com>
2025-03-18 20:58:55 +0700

    in charge ke web.php

--- rentalmobil/routes/web.php
+++ rentalmobil/routes/web.php
@@ -8,13 +8,14 @@ use App\Http\Controllers\LoginController;
 // Route untuk LoginController
 Route::get('/login', [LoginController::class, 'index']);
 
-// Route untuk ItemsController
-Route::get('/items', [ItemController::class, 'index']);
-
 // Route untuk DashboardController
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+// Route untuk ItemsController
+Route::get('/items', [ItemController::class, 'index']);
 
 // Route default Laravel
 Route::get('/', function () {
     return view('welcome');
 });
+
