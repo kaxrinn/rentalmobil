7e5bbf3093e80730da94322051e7ef262a8d8a8d
titho1071 <tithoilham@gmail.com>
2025-05-10 06:26:59 +0700

    beranda admin,pemesanan admin, mobil admin titho commit

--- rentalmobil/app/Http/Controllers/AdminController.php
+++ rentalmobil/app/Http/Controllers/AdminController.php
@@ -6,7 +6,26 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function index() {
-        return view('landingadmin');
+    public function index()
+    {
+        return view('pages.admin');
+    }
+
+    public function pemesananadmin()
+    {
+        return view('pages.pemesananadmin');
+    }
+    public function mobiladmin()
+    {
+        return view('pages.mobiladmin');
+    }
+    public function ulasanadmin()
+    {
+        return view('pages.ulasanadmin');
+    }
+    public function hubungiadmin()
+    {
+        return view('pages.hubungiadmin');
     }
 }
+
--- rentalmobil/routes/web.php
+++ rentalmobil/routes/web.php
@@ -1,84 +1,10 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HistoryController;
-
-// Route untuk pemesanan
-Route::get('/pemesanan', function () {
-    return view('pemesanan');
-})->name('pemesanan');
-
-// Route untuk riwayat
-Route::get('/riwayat', function () {
-    return view('riwayat');
-})->name('riwayat');
-
-// Route untuk search
-Route::get('/search', function () {
-    return view('search');
-})->name('search');
-
-// Route untuk beranda
-Route::get('/beranda', function () {
-    return view('beranda');
-})->name('beranda');
-
-// Route untuk hubungi_kami
-Route::get('/hubungi_kami', function () {
-    return view('hubungi_kami');
-})->name('hubungi_kami');
-
-// Route untuk landingpage
-Route::get('/landingpage', function () {
-    return view('landingpage');
-})->name('landingpage');
-
-// Route untuk halaman registrasi
-Route::get('/register', function () {
-    return view('register');
-})->name('register');
-
-// Route untuk login
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-// Route untuk mobil
-Route::get('/mobil', function () {
-    return view('mobil');
-})->name('mobil');
-
-// Route untuk lupa sandi
-Route::get('/lupasandi', function () {
-    return view('lupasandi');
-})->name('lupasandi');
-
-// Route untuk landingpage_before
-Route::get('/landingpage_before', function () {
-    return view('landingpage_before');
-})->name('landingpage_before');
-
-// Route untuk ulasan
-Route::get('/ulasan', function () {
-    return view('ulasan');
-})->name('ulasan');
-
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-Route::get('/edit_profile', function () {
-    return view('edit_profile');
-})->name('edit_profile');
-
-Route::get('/detail', function () {
-    return view('detail');
-})->name('detail');
-
-
+use App\Http\Controllers\AdminController;
 
 // Halaman Utama
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -95,3 +21,15 @@ Route::get('/riwayat', [HistoryController::class, 'index'])->name('riwayat');
 
 // Logout
 Route::post('/logout', [ProfileController::class, 'logout'])->name('logout');
+
+//route untuk halaman admin page
+
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+
+Route::get('/pemesananadmin', [AdminController::class, 'pemesananAdmin'])->name('pemesananadmin');
+
+Route::get('/mobiladmin', [AdminController::class, 'mobiladmin'])->name('mobiladmin');
+
+Route::get('/ulasanadmin', [AdminController::class, 'ulasanadmin'])->name('ulasanadmin');
+
+Route::get('/hubungiadmin', [AdminController::class, 'hubungiadmin'])->name('hubungiadmin');
