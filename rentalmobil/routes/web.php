cde3b4976f990c5282be56f6b8098829c235d2b2
lathifah199 <lathifahkhalifatunnisa.com>
2025-06-03 07:52:22 +0700

    tambah sedikit web.php

--- rentalmobil/routes/web.php
+++ rentalmobil/routes/web.php
@@ -2,7 +2,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingpageController;
 use App\Http\Controllers\LandingpagebfController;
-
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HistoryController;
 use App\Http\Controllers\RiwayatController;
@@ -10,6 +10,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ForgotPasswordController;
 use App\Http\Controllers\EditProfileController;
+use App\Http\Controllers\MobilController;
 use App\Http\Controllers\PesanController;
 
 
@@ -19,6 +20,7 @@ Route::get('/landingpage', [LandingpageController::class, 'index'])->name('landi
 Route::get('/landingpagebf', [LandingpagebfController::class, 'index'])->name('landingpagebf');
 
 // Kontak
+Route::post('/contact', [ContactController::class, 'submit'])->name('contact.submit');
 
 // Riwayat
 Route::get('/riwayat', [RiwayatController::class, 'index'])->name('riwayat');
@@ -31,7 +33,22 @@ Route::get('/admin', [AdminController::class, 'index'])->name('admin');
 
 Route::get('/pemesananadmin', [AdminController::class, 'pemesananAdmin'])->name('pemesananadmin');
 
-Route::get('/mobiladmin', [AdminController::class, 'mobiladmin'])->name('mobiladmin');
+
+Route::prefix('mobiladmin')->group(function () {
+    Route::get('/', [MobilController::class, 'index'])->name('mobiladmin');
+    Route::get('/{kode_mobil}/edit', [MobilController::class, 'edit'])->name('mobil.edit');
+    Route::post('/', [MobilController::class, 'store'])->name('mobil.store');
+    Route::put('/{kode_mobil}', [MobilController::class, 'update'])->name('mobil.update');
+    Route::delete('/{kode_mobil}', [MobilController::class, 'destroy'])->name('mobil.destroy');
+    Route::get('/debug-storage', [MobilController::class, 'debugStorage']);
+});
+Route::get('/storage/{path}', function ($path) {
+    $file = storage_path('app/public/' . $path);
+    if (file_exists($file)) {
+        return response()->file($file);
+    }
+    abort(404);
+})->where('path', '.*');
 
 Route::get('/ulasanadmin', [AdminController::class, 'ulasanadmin'])->name('ulasanadmin');
 
@@ -53,3 +70,14 @@ Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLink
 Route::get('/edit-profile', [EditProfileController::class, 'show'])->name('edit-profile');
 Route::post('/edit-profile', [EditProfileController::class, 'update'])->name('edit-profile.update');
 
+// Form untuk user kirim pesan
+Route::get('/contact', [PesanController::class, 'form'])->name('contact.form');
+
+// Proses submit form
+Route::post('/kirim-pesan', [PesanController::class, 'submit'])->name('contact.submit');
+
+// Halaman admin untuk melihat daftar pesan
+Route::get('/hubungiadmin', [PesanController::class, 'index'])->name('hubungiadmin');
+
+// Admin hapus pesan
+Route::delete('/admin/hubungi/{id}', [PesanController::class, 'destroy'])->name('admin.hapuspesan');
